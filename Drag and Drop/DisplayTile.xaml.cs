b77ca09235a21d7fd790be8fbd31e7ff1d93e1fe
TacticDevs <jamiesaidi@outlook.com>
2022-07-27 00:30:50 +0200

    Version 1.0

---
+++ Drag and Drop/DisplayTile.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Drag_and_Drop
+{
+    /// <summary>
+    /// Interaction logic for DisplayTile.xaml
+    /// </summary>
+    public partial class DisplayTile : UserControl
+    {
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+        public static readonly DependencyProperty TextProperty =DependencyProperty.Register("Text", typeof(string), typeof(DisplayTile), new PropertyMetadata(null));
+
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+        public static readonly DependencyProperty ImageProperty =DependencyProperty.Register("Image", typeof(ImageSource), typeof(DisplayTile), new PropertyMetadata(null));
+
+
+
+        public DisplayTile()
+        {
+            InitializeComponent();
+            DataContext = this;           
+        }
+
+
+
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+
+                DragDrop.DoDragDrop(this, this, DragDropEffects.Move);
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Drag and Drop/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drag_and_Drop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TargetPanel_Drop(object sender, DragEventArgs e)
+        {
+            var obj = e.Data.GetData(typeof(DisplayTile)) as DisplayTile;
+
+            ((WrapPanel)(obj.Parent)).Children.Remove(obj);
+            TargetPanel.Children.Add(obj);
+
+        }
+    }
+}
+  
